6a01f0455e97b6a399e6d508e305018d6f5411fc
kpotoh <axepon@mail.ru>
2022-02-11 16:53:29 +0200

    saturday tasks specified

---
+++ src/prepare_constrain_tree.py
@@ -0,0 +1,6 @@
+"""
+TODO
+- check orders
+- dict[order: [species]]
+- replace order in readed newick  # orner_name --> (...)
+"""
---
+++ src/prepare_fasta_to_aln.py
@@ -0,0 +1,7 @@
+"""
+TODO
+- sort dataframe
+- drop ND6
+- replace \s to _ in names
+- write 12 files with nucleotides
+"""
