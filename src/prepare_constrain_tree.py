6250def56ad7b6fcf4fe283cb8d9202659df9e00
kpotoh <axepon@mail.ru>
2022-02-12 15:27:25 +0200

    first meeting

--- src/prepare_constrain_tree.py
+++ src/prepare_constrain_tree.py
@@ -1,6 +1,71 @@
 """
 TODO
-- check orders
-- dict[order: [species]]
-- replace order in readed newick  # orner_name --> (...)
++ check orders
++ dict[order: [species]]
++ replace order in readed newick  # orner_name --> (...)
+- update odrer tree
 """
+
+from collections import defaultdict
+import re
+
+import pandas as pd
+
+PATH_TO_DATA = "./data/raw/final_birds_list_with_no_mistakes.csv"
+PATH_TO_CONSTRAIN = "./data/external/constraint/orders.tre"
+
+
+def read_data(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path, index_col=0).drop("X", axis=1)
+    orders = df[["Species.name", "Taxonomy"]]
+    orders["Species.name"] = orders["Species.name"].str.replace(" ", "_")
+    orders["Taxonomy"] = orders.Taxonomy.apply(eval)
+    assert len(set(orders.Taxonomy.apply(lambda x: x[12]))) == 1  # birds
+    assert "Passeriformes" in set(orders.Taxonomy.apply(lambda x: x[14]))
+    orders["Order"] = orders.Taxonomy.apply(lambda x: x[14])
+    return orders
+
+
+def df2dict(df: pd.DataFrame) -> dict:
+    assert "Species.name" in df.columns and "Order" in df.columns
+    dct = defaultdict(list)
+    for name, order in df[["Species.name", "Order"]].values:
+        dct[order].append(name)
+    return dct
+
+
+def read_constraint_tree(path: str) -> str:
+    with open(path) as fin:
+        tree = fin.read().strip()
+    return tree
+
+
+def add_species2tree(constraint_tree, order2species) -> str:
+    orders_from_tree = re.findall("(\w+)", constraint_tree)
+    orders_from_data = set(order2species.keys())
+
+    for ordr in orders_from_tree:
+        if ordr in orders_from_data:
+            species = order2species[ordr]
+            sp_str = ",".join(species)
+            clade = "({})".format(sp_str)
+            constraint_tree = constraint_tree.replace(ordr, clade)
+        else:
+            constraint_tree = constraint_tree.replace(ordr, "NNNNN")
+    return constraint_tree
+
+
+def main():
+    orders = read_data(PATH_TO_DATA)
+    order2species = df2dict(orders)
+    constraint_tree = read_constraint_tree(PATH_TO_CONSTRAIN)
+
+    # assert set(order2species.keys()).  # TODO extend tree to our data
+
+    updated_tree = add_species2tree(constraint_tree, order2species)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
--- src/prepare_fasta_to_aln.py
+++ src/prepare_fasta_to_aln.py
@@ -1,7 +1,52 @@
 """
 TODO
-- sort dataframe
-- drop ND6
-- replace \s to _ in names
-- write 12 files with nucleotides
++ sort dataframe
++ replace \s to _ in names
++ drop ND6
++ write 12 files with nucleotides
 """
+import os
+import pandas as pd
+
+PATH_TO_DATA = "./data/raw/final_birds_list_with_no_mistakes.csv"
+PATH_TO_OUT_DIR = "./data/interim/gene_seqs/"
+
+
+def read_data(path: str) -> pd.DataFrame:
+    df = pd.read_csv(path, index_col=0).drop("X", axis=1)
+    seqs = df[["Species.name", "Gene.name", "Sequence"]]
+    seqs["Species.name"] = seqs["Species.name"].str.replace(" ", "_")
+    seqs = seqs.sort_values("Species.name")
+    seqs["Gene.name"] = seqs["Gene.name"].str.extract("\[(.+)\]")
+    seqs = seqs[seqs["Gene.name"] != "ND6"]
+    return seqs
+
+
+def file_write(data: pd.DataFrame, path_to_out: str):
+    genes = data["Gene.name"].unique()
+    assert os.path.exists(path_to_out), f"{path_to_out} doesn't exist"
+    fouts = [open(f"{path_to_out}{gn}.fasta", "w") for gn in genes]
+
+    for i, gn in enumerate(genes):
+        cur_seqs = data[data["Gene.name"] == gn]
+        cur_fout = fouts[i]
+        for sp_name, seq in cur_seqs[["Species.name", "Sequence"]].values:
+            cur_fout.write(f">{sp_name}\n")
+            cur_fout.write(f"{seq}\n")
+
+    for fout in fouts:
+        fout.close()
+
+
+def main():
+    seqs = read_data(PATH_TO_DATA)
+    assert all(seqs["Gene.name"] != "ND6"), "col Gene.name contains gene ND6"
+
+    if not os.path.exists(PATH_TO_OUT_DIR):
+        os.mkdir(PATH_TO_OUT_DIR)
+
+    file_write(seqs, PATH_TO_OUT_DIR)
+
+
+if __name__ == "__main__":
+    main()
